222c6549a060be6441e3e437146657e2fa3626e8
vishchak <denis.vishchak@gmail.com>
2022-11-18 12:28:49 +1100

    initial commit;

---
+++ src/ua/kiev/prog/Main.java
@@ -0,0 +1,40 @@
+package ua.kiev.prog;
+
+import ua.kiev.prog.exceptions.NoLoginException;
+import ua.kiev.prog.registration.Register;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            String login = String.valueOf(Register.register(sc));
+
+            Thread th = new Thread(new GetThread());
+            th.setDaemon(true);
+            th.start();
+
+            System.out.println("Enter your message: ");
+            while (true) {
+                String text = sc.nextLine();
+                if (text.isEmpty()) break;
+
+                Message m = new Message(login, text);
+                int res = m.send(Utils.getURL() + "/add");
+
+                if (res != 200) { // 200 OK
+                    System.out.println("HTTP error occured: " + res);
+                    return;
+                }
+            }
+        } catch (NoLoginException e) {
+            e.getMessage();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            sc.close();
+        }
+    }
+}
